def37695e45ea175c969497f80e57d1e8d2ec930
Fekete Viktor <kurvanev@gmail.com>
2019-02-17 11:10:48 +0100

    Modified skill format.

--- scripts/generate-skills.js
+++ scripts/generate-skills.js
@@ -6,17 +6,10 @@ let skills = fs.readFileSync(inPath, 'UTF-8')
 let lines = skills.split('\n')
 
 const skillsArray = []
-const levelPattern = /\/\/(\d+)/
-let currentLevel = -1
 
 for (line of lines) {
-  let match = line.match(levelPattern)
-  if (match) {
-    currentLevel = match[1]
-  } else {
-    let [name, details] = line.split('|').map(s => s.trim())
-    skillsArray.push({ name, exposure: currentLevel, details })
-  }
+  let [name, level, details] = line.split('|').map(s => s.trim())
+  skillsArray.push({ name, exposure: level, details })
 }
 
 let output = JSON.stringify(skillsArray, null, 2);
